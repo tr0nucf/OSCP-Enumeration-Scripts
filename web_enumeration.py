093d16941477812825dcfaa61769695e11f1e658
Cameron Edmondson <42309690+cdedmondson@users.noreply.github.com>
2020-03-28 13:37:11 -0700

    Update web_enumeration.py

--- web_enumeration.py
+++ web_enumeration.py
@@ -1,18 +1,68 @@
 import subprocess
+import socket
+
+
+def is_valid_ip():
+    global ip
+    while True:
+        try:
+            ip = input("[*] Enter ip of host:")
+            if socket.gethostbyname(ip) == ip:
+                break
+        except socket.gaierror:
+            print("Something is wrong")
+
+    return ip
+
+
+def is_valid_port():
+    global port
+    while True:
+        try:
+            port = int(input("[*] Enter port of host:"))
+        except ValueError:
+            print("Error: expect an integer. Try again.")
+            continue
+        if 1 <= port <= 65535:
+            break
+        else:
+            print("Port must between range 1 <= port <= 65535")
+            continue
+
+    return port
+
+
+def port_443(port):
+    if port == 443:
+        return True
+    else:
+        return False
 
 
 def whatweb(ip, port):
-    whatweb = f"whatweb {ip}:{port}"
+    if not port_443(port):
+        whatweb = f"whatweb -v -a 1 http://{ip}:{port}"
+    else:
+        whatweb = f"whatweb -v -a 1 https://{ip}:{port}"
+
     subprocess.run(whatweb, shell=True)
 
 
 def dirsearch(ip, port):
-    dirsearch = f"/opt/dirsearch/./dirsearch.py -u {ip}:{port} -e php,asp,aspx,sh,py,rb,pl,jsp -r"
+    if not port_443(port):
+        dirsearch = f"/opt/dirsearch/./dirsearch.py -u http://{ip}:{port} -e php,asp,aspx,sh,py,rb,pl,jsp -r"
+    else:
+        dirsearch = f"/opt/dirsearch/./dirsearch.py -u https://{ip}:{port} -e php,asp,aspx,sh,py,rb,pl,jsp -r"
+
     subprocess.run(dirsearch, shell=True)
 
 
 def gobuster(ip, port):
-    gobuster = f"gobuster dir -u http://{ip}:{port}/ -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -x php -o gobuster-root -t 20"
+    if not port_443(port):
+        gobuster = f"gobuster dir -u http://{ip}:{port} -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -x php -o gobuster-root -t 20"
+    else:
+        gobuster = f"gobuster dir -u https://{ip}:{port} -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -x php -o gobuster-root -t 20 -k"
+
     subprocess.run(gobuster, shell=True)
 
 
@@ -22,7 +72,11 @@ def nikto(ip, port):
 
 
 def dirb(ip, port):
-    dirb = f"dirb http://{ip}:{port}"
+    if not port_443(port):
+        dirb = f"dirb http://{ip}:{port}"
+    else:
+        dirb = f"dirb https://{ip}:{port}"
+
     subprocess.run(dirb, shell=True)
 
 
@@ -36,8 +90,8 @@ def scan(ip, port):
 
 def main():
     # Prompt for ip input
-    ip = input("[*] Enter ip of host:")
-    port = input("[*] Enter port of host:")
+    ip = is_valid_ip()
+    port = is_valid_port()
     # Invoke whatweb, nikto, dirsearch, gobuster, dirb
     scan(ip, port)
 
